1400446f9ea1ee77b2b30867a57f9b71b4270e46
Dilip <32507121+Dilipjp@users.noreply.github.com>
2022-10-25 00:16:42 +0530

    remove all

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package io.ionic.starter;
-
-import com.getcapacitor.BridgeActivity;
-import ch.byrds.capacitor.contacts.Contacts;
-
-public class MainActivity extends BridgeActivity {
-    // Initializes the Bridge
-
-}
